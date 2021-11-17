8ce75931c29ba4c33e59b3d6f28a2f4b7b8bc12c
Pavel Grudinskiy <mr.pv234@mail.ru>
2021-11-17 12:49:05 +0300

    Добавьте файлы проекта.

---
+++ LibrariesCreator/Diff.cpp
@@ -0,0 +1,6 @@
+#include "Diff.hpp"
+
+int diff(int a, int b)
+{
+	return a - b;
+}
---
+++ LibrariesCreator/Mult.cpp
@@ -0,0 +1,9 @@
+﻿// LibrariesCreator.cpp: определяет точку входа для приложения.
+//
+
+#include "Mult.hpp"
+
+int mult(int a, int b)
+{
+	return a * b;
+}
